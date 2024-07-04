ac19dc371b722503658728a6b516c58026dc7d8a
nandhini6132 <nandhini6132@gmail.com>
2024-07-04 16:32:51 +0530

    jlk

--- src/database/index.js
+++ src/database/index.js
@@ -5,9 +5,11 @@ import mongoose from 'mongoose';
 
 const connectToDb=async()=>{
     try {
-        await mongoose.connect('mongodb+srv://Nandhini:Elakkiya@cluster0.7ucexvj.mongodb.net/leetcoderetryWrites=true&w=majority', {
+        await mongoose.connect('mongodb+srv://Nandhini:Elakkiya@cluster0.7ucexvj.mongodb.net/leetcode',{
             useNewUrlParser: true,
             useUnifiedTopology: true,
+            useFindAndModify: false,
+            useCreateIndex: true,
           })
         console.log('connected to db')
     } catch (error) {
